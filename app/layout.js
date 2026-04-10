2549d0e9cf5a161359345089e34487d1b248ce4d
young-gre <yrson916@gmail.com>
2026-04-10 11:31:59 +0900

    충북 조건 검색기 v1.3.4

--- app/layout.js
+++ app/layout.js
@@ -4,26 +4,6 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ko">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-      </head>
-      <body style={{
-        margin: 0,
-        fontFamily: "'Noto Sans KR', 'Apple SD Gothic Neo', sans-serif",
-        background: '#f6f3f2',
-        color: '#1a1a1a'
-      }}>
-        {children}
-      </body>
-    </html>
-  )
-}
-
-export default function RootLayout({ children }) {
 return (
 <html lang="ko">
 <head>
